cab376f61afc292cf18374105b9452b32e3ac70a
ZAM <ZAM@1CLICK.LV>
2018-11-13 20:55:41 +0200

    Improvements in style for rotated column header

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,8 +16,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using PdfAuthorVerifier.Functions;
-using PdfAuthorVerifier.Types;
 using PdfAuthorVerifier.ViewModels;
 
 namespace PdfAuthorVerifier
